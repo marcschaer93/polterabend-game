862fe316a589fa45f12255613233e561a7fa6b71
Marc Schär <marc.schaer93@gmail.com>
2025-04-25 09:10:48 +0200

    Erster Commit

---
+++ main.js
@@ -0,0 +1,8 @@
+// Hier initialisierst du Deck.js und definierst dein Spiel
+document.addEventListener("DOMContentLoaded", function () {
+  var deck = new Deck();
+  // Hier kannst du die Karten für dein Polterabend-Spiel hinzufügen
+  deck.addCard("Trink einen Shot");
+  deck.addCard("Mache 10 Hampelmänner");
+  // Weitere Karten und Logik hinzufügen
+});
